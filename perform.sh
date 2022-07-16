d09d899b899014de49bc96e3755f3243ff62cfb3
rirl <rlandon@usa.net>
2022-07-15 17:52:14 -0700

    chore: Make perform output more understandable.

--- perform.sh
+++ perform.sh
@@ -6,20 +6,25 @@ echo "[$0]:: Begin [$*]"
 echo "[$0]:: Show information..."
 kubectl get deployment webdeploy
 kubectl get service webdeploy-service
+read
+clear
 
 #echo "[$0]:: Service URL ..."
 #minikube service webdeploy-service --url
 
-echo "[$0]:: Scaling ..."
+echo "[$0]:: Scaling Up...[5]"
 kubectl scale --replicas=5 deployment/webdeploy
 kubectl get deployment webdeploy
 read
+clear
 
+echo "[$0]:: Scaling Down...[3]"
 kubectl scale --replicas=3 deployment/webdeploy
 kubectl get deployment webdeploy
 read
+clear
 
-echo "[$0]:: Rollout ..."
+echo "[$0]:: Rollout ...[v2]"
 kubectl describe deployment webdeploy
 kubectl describe service    webdeploy-service
 kubectl set image deployments/webdeploy webdeploy=rirl/webdeploy:v2
@@ -31,6 +36,7 @@ kubectl describe deployment webdeploy
 echo "[$0]:: Rollback ..."
 kubectl set image deployments/webdeploy webdeploy=rirl/webdeploy:v1
 read
+clear
 
 echo "[$0]:: Post Rollback ..."
 kubectl describe deployment webdeploy
